28ae4aad433c7b0bf2273a4652ff74706e24f015
ainoue1995 <inoueakio0205@gmail.com>
2021-11-16 02:19:14 +0900

    update to install rosseta for google-japanese-ime

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -eu
 # 実行場所のディレクトリを取得
 THIS_DIR=$HOME/dotfiles
 
+# ロゼッタのインストール
+sudo softwareupdate --install-rosetta
+
 if [ ! -d "$THIS_DIR" ]; then
     git clone https://github.com/ainoue1995/dotfiles.git "$THIS_DIR"
 else
